6bd55415fcade9c0a94e55ccf9be78bcbe859d33
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2022-11-05 20:52:36 +0100

    setup express

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-"use strict";
+const express = require("express");
+const dotenv = require("dotenv");
 
-console.log("Hello typescript, I missed you 😍");
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/healthcheck", (req: any, res: any) => {
+  res.send("wallet is up and running");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
