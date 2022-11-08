b71ef533d6d6e5e6fd82fb9b5b60a585b7340cf7
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2022-11-08 07:48:54 +0100

    add tokenization for cards and caching for bankaccount

--- src/services/users/users.service.ts
+++ src/services/users/users.service.ts
@@ -1,10 +1,15 @@
 import chalk from "chalk";
 import { knex as dbClient } from "../../configs/knex";
 import { logger } from "../../configs/logger";
-import { WalletFundDto, WalletTransferDto, WalletWithdrawalDto } from "./dtos/wallet-transfer.dto";
+import { PayeeBankAccountDetailsType, PayerCardDetailsType, WalletFundDto, WalletTransferDto, WalletWithdrawalDto } from "./dtos/wallet-transfer.dto";
 import { fundWalletWithCard, transferFunds, withdrawFromWalletToBank } from "../wallet/wallet.service";
 import { Wallet } from "../../models/wallet";
 import { BankAccount } from "../../models/bank_account";
+import { v4 as uuidv4 } from "uuid";
+import { Knex } from "knex";
+import { Entity } from "../../models/entity";
+import { format } from "date-fns";
+import { Card } from "../../models/card";
 
 export async function executeWalletTransfer(walletTransferDto: WalletTransferDto, userUuid: string, walletUuid: string) {
   const trx = await dbClient.transaction();
@@ -49,9 +54,7 @@ export async function fundWallet(walletFundDto: WalletFundDto, userUuid: string,
       .whereNotNull("disabled_date");
     if (!deepUserRecord.length || deepUserRecord[0].wallet.uuid !== walletUuid) throw new Error("no wallet found");
 
-    // TODO: tokenize card if new card is provided
-    const card = { id: 1 };
-
+    const card = await tokenizeOrfetchCard(userUuid, trx, walletFundDto.PayerEntityUuid, walletFundDto.PayerCardDetails);
     await fundWalletWithCard(card, deepUserRecord[0].wallet.id, walletFundDto.Amount, trx);
 
     trx.commit();
@@ -78,9 +81,7 @@ export async function withdrawFromWallet(walletWithdrawalDto: WalletWithdrawalDt
       .whereNotNull("disabled_date");
     if (!deepUserRecord.length || deepUserRecord[0].wallet.uuid !== walletUuid) throw new Error("no wallet found");
 
-    // TODO: tokenize bank if new one is provided
-    const bankAccount: BankAccount = new BankAccount();
-
+    const bankAccount = await fetchAccount(userUuid, trx, walletWithdrawalDto.PayeeEntityUuid, walletWithdrawalDto.PayeeBankAccountDetails);
     await withdrawFromWalletToBank(deepUserRecord[0].wallet, bankAccount, walletWithdrawalDto.Amount, trx);
 
     trx.commit();
@@ -92,4 +93,81 @@ export async function withdrawFromWallet(walletWithdrawalDto: WalletWithdrawalDt
   }
 
   return { success: false };
+}
+
+async function fetchAccount(userUuid: string, trx: Knex, accountUuid?: string, payeeBankAccountDetailsType?: PayeeBankAccountDetailsType) {
+  let bankAccount: BankAccount;
+  const timestamp = format(new Date(), "yyyy-MM-dd HH:mm:ss");
+
+  if (!accountUuid) {
+    const [entity] = await trx<Entity>("entity")
+      .insert({
+        uuid: uuidv4(),
+        entity_type_id: 3, // bank_account
+        created_at: timestamp,
+        last_updated_at: timestamp,
+      })
+      .returning("id");
+
+    const [ownerEntity] = await trx<Entity>("entity").select("id").where({ uuid: userUuid });
+    if (!ownerEntity) throw new Error(`unable to find owner entity record for userUuid: ${userUuid}`);
+
+    bankAccount = (
+      await trx<BankAccount>("bank_account")
+        .insert({
+          uuid: uuidv4(),
+          entity_id: entity.id,
+          owner_entity_id: ownerEntity.id,
+          account_number: payeeBankAccountDetailsType?.AccountNumber,
+        })
+        .returning("*")
+    )[0];
+  } else {
+    bankAccount = (await trx<BankAccount>("bank_account").select().where({ uuid: accountUuid }).whereNotNull("disabled_date"))[0];
+  }
+
+  if (!bankAccount) throw new Error("invalid bank account supplied");
+
+  return bankAccount;
+}
+
+async function tokenizeOrfetchCard(userUuid: string, trx: Knex, accountUuid?: string, payerCardDetailsType?: PayerCardDetailsType) {
+  let card: Card;
+  const timestamp = format(new Date(), "yyyy-MM-dd HH:mm:ss");
+
+  if (!accountUuid) {
+    const [entity] = await trx<Entity>("entity")
+      .insert({
+        uuid: uuidv4(),
+        entity_type_id: 4, // card
+        created_at: timestamp,
+        last_updated_at: timestamp,
+      })
+      .returning("id");
+
+    const [ownerEntity] = await trx<Entity>("entity").select("id").where({ uuid: userUuid });
+    if (!ownerEntity) throw new Error(`unable to find owner entity record for userUuid: ${userUuid}`);
+
+    card = (
+      await trx<Card>("card")
+        .insert({
+          uuid: uuidv4(),
+          entity_id: entity.id,
+          owner_entity_id: ownerEntity.id,
+          cardholder_firstName: payerCardDetailsType?.CardholderFirstName,
+          cardholder_lastName: payerCardDetailsType?.CardholderLastName,
+          card_cvv: payerCardDetailsType?.Cvv,
+          card_expiration: payerCardDetailsType?.ExpirationDate,
+          card_number: payerCardDetailsType?.CardNumber,
+          card_pin: payerCardDetailsType?.Pin,
+        })
+        .returning("*")
+    )[0];
+  } else {
+    card = (await trx<Card>("card").select().where({ uuid: accountUuid }).whereNotNull("disabled_date"))[0];
+  }
+
+  if (!card) throw new Error("invalid card details supplied");
+
+  return card;
 }
