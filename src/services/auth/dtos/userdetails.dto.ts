d28da97605da7a92568b6bace22afbf1f9f93d11
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2022-11-08 05:39:18 +0100

    bootstrap validator for auth and wallet

--- src/services/auth/dtos/userdetails.dto.ts
+++ src/services/auth/dtos/userdetails.dto.ts
@@ -1,6 +1,50 @@
-export interface UserDetailsDto {
+import { IsEmail, IsOptional, IsString, IsNotEmpty, MinLength, MaxLength, validate, ValidationError } from "class-validator";
+import { Response } from "express";
+import { Request } from "express-jwt";
+
+export class UserDetailsDto {
+  @IsOptional()
+  @IsString()
   FirstName?: string;
+
+  @IsOptional()
+  @IsString()
   LastName?: string;
+
+  @IsEmail()
   Email: string;
+
+  @MaxLength(50, { message: "password is too long" })
+  @MinLength(8, { message: "password is too short" })
+  @IsNotEmpty()
   Password: string;
+
+  constructor(params: { Email: string; Password: string; FirstName?: string; LastName?: string }) {
+    this.Email = params.Email;
+    this.Password = params.Password;
+    this.FirstName = params.FirstName;
+    this.LastName = params.LastName;
+  }
+}
+
+export function validateWithPromise<T extends Object>(schema: T) {
+  return new Promise((resolve) => {
+    validate(schema, { forbidUnknownValues: true }).then((errors) => {
+      resolve(errors);
+    });
+  });
+}
+
+export async function validateWrapper<T extends Object>(Schema: T, req: Request, res: Response) {
+  const errors = (await validateWithPromise(Schema)) as ValidationError[];
+  if (errors.length) {
+    res.status(400).json({
+      status: 400,
+      message: errors.map((error) => Object.values(error.constraints || {})).join(";"),
+    });
+
+    return false;
+  }
+
+  return true;
 }
---
+++ src/services/users/dtos/wallet-transfer.dto.ts
@@ -0,0 +1,110 @@
+import { Type } from "class-transformer";
+import { IsDateString, IsOptional, IsPositive, IsString, IsUUID, Min, ValidateIf, ValidateNested } from "class-validator";
+import 'reflect-metadata';
+
+export class WalletTransferDto {
+  @IsUUID()
+  RecipientWalletId: string;
+
+  @IsPositive()
+  @Min(100) // all arithmetic operations will be done in lower unit (kobo) and minimum value we'll accept is 1 naira
+  @Type(() => Number)
+  Amount: number;
+
+  constructor(params: { RecipientWalletId: string; Amount: number }) {
+    this.RecipientWalletId = params.RecipientWalletId;
+    this.Amount = params.Amount;
+  }
+}
+
+export class WalletFundDto {
+  @IsPositive()
+  @Min(100)
+  @Type(() => Number)
+  Amount: number;
+
+  @ValidateIf((obj: WalletFundDto) => !obj.PayerCardDetails)
+  @IsOptional()
+  @IsUUID()
+  PayerEntityUuid?: string; // for previously tokenized cards
+
+  @ValidateIf((obj: WalletFundDto) => !obj.PayerEntityUuid)
+  @IsOptional()
+  @ValidateNested()
+  PayerCardDetails?: PayerCardDetailsType;
+
+  constructor(params: { PayerEntityUuid: string; Amount: number; PayerCardDetails: PayerCardDetailsType }) {
+    this.PayerEntityUuid = params.PayerEntityUuid;
+    this.PayerCardDetails = new PayerCardDetailsType({ ...params.PayerCardDetails });
+    this.Amount = params.Amount;
+  }
+}
+
+export class PayerCardDetailsType {
+  @IsString()
+  CardholderFirstName: string;
+
+  @IsString()
+  CardholderLastName: string;
+
+  @IsString()
+  CardNumber: string;
+
+  @IsString()
+  Cvv: string;
+
+  @IsString()
+  Pin: string;
+
+  @IsDateString()
+  ExpirationDate: string;
+
+  constructor(params: { CardholderFirstName: string; CardholderLastName: string; CardNumber: string; Cvv: string; Pin: string; ExpirationDate: string }) {
+    this.CardholderFirstName = params.CardholderFirstName;
+    this.CardholderLastName = params.CardholderLastName;
+    this.CardNumber = params.CardNumber;
+    this.Cvv = params.Cvv;
+    this.Pin = params.Pin;
+    this.ExpirationDate = params.ExpirationDate;
+  }
+}
+
+export class WalletWithdrawalDto {
+  @IsPositive()
+  @Min(100)
+  @Type(() => Number)
+  Amount: number;
+
+  @ValidateIf((obj: WalletWithdrawalDto) => !obj.PayeeBankAccountDetails)
+  @IsOptional()
+  @IsUUID()
+  PayeeEntityUuid?: string; // for previously tokenized accounts
+
+  @ValidateIf((obj: WalletWithdrawalDto) => !obj.PayeeEntityUuid)
+  @IsOptional()
+  @ValidateNested()
+  PayeeBankAccountDetails?: PayeeBankAccountDetailsType;
+
+  constructor(params: { PayeeEntityUuid: string; Amount: number; PayeeBankAccountDetails: PayeeBankAccountDetailsType }) {
+    this.PayeeEntityUuid = params.PayeeEntityUuid;
+    this.PayeeBankAccountDetails = new PayeeBankAccountDetailsType({ ...params.PayeeBankAccountDetails });
+    this.Amount = params.Amount;
+  }
+}
+
+export class PayeeBankAccountDetailsType {
+  @IsString()
+  AccountNumber: string;
+
+  @IsString()
+  BankName: string;
+
+  @IsString()
+  BankAccountType: string;
+
+  constructor(params: { AccountNumber: string; BankName: string; BankAccountType: string }) {
+    this.AccountNumber = params.AccountNumber;
+    this.BankName = params.BankName;
+    this.BankAccountType = params.BankAccountType;
+  }
+}
