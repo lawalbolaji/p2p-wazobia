0f19a3e3aff443dc77aaf3ff2c7d7b8741569470
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2023-04-17 16:28:05 +0100

    feature (wallet): delegate wallet CRUD to wallet service

--- src/modules/wallet/services/wallet.service.ts
+++ src/modules/wallet/services/wallet.service.ts
@@ -1,9 +1,37 @@
+import { Knex } from "knex";
+import { Wallet } from "../../../db_models/wallet";
+import { v4 as uuidv4 } from "uuid";
+
 export class WalletService {
-  constructor() {}
+  constructor(private readonly dbClient: Knex) {}
 
   getWalletBalance(walletId: string) {}
 
-  getUserWallets(userId: string) {}
+  async getWalletByOwnerEntityId(owner_entity_id: number) {
+    const [wallet] = await this.dbClient<Wallet>("wallet").where("owner_entity_id", owner_entity_id);
 
-  createNewUserWallet(userId: string) {}
+    return wallet;
+  }
+
+  createNewUserWallet(owner_entity_id: number, trx: Knex) {
+    const wallet_uuid = uuidv4();
+
+    return trx<Wallet>("wallet").insert({
+      uuid: wallet_uuid,
+      balance: 0,
+      owner_entity_id,
+    });
+  }
+
+  addMoneyToWallet(amount: number, walletId: number, trx: Knex) {
+    trx<Wallet>("wallet")
+      .update({ balance: trx.raw(`balance + ${amount}`) })
+      .where({ id: walletId });
+  }
+
+  takeMoneyFromWallet(amount: number, walletId: number, trx: Knex) {
+    trx<Wallet>("wallet")
+      .update({ balance: trx.raw(`balance - ${amount}`) })
+      .where({ id: walletId });
+  }
 }
