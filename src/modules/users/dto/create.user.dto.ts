6752e15bf306bd9b24f2d0f5266edebeb676f063
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2023-04-18 14:14:22 +0100

    security: remove permissionflags from create-user payload

--- src/modules/users/dto/create.user.dto.ts
+++ src/modules/users/dto/create.user.dto.ts
@@ -13,20 +13,10 @@ export class CreateUserDto {
   @IsOptional()
   lastName?: string;
 
-  @IsOptional()
-  permissionFlags?: number;
-
-  constructor(params: {
-    email: string;
-    password: string;
-    firstName?: string;
-    lastName?: string;
-    permissionFlags?: number;
-  }) {
+  constructor(params: { email: string; password: string; firstName?: string; lastName?: string; permissionFlags?: number }) {
     this.email = params.email;
     this.password = params.password;
     this.firstName = params.firstName;
     this.lastName = params.lastName;
-    this.permissionFlags = params.permissionFlags;
   }
 }
