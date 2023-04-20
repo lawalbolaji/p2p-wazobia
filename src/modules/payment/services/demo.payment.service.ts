521f757bd144ea9aaaa27f5f8c998853782a8a8d
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2023-04-20 15:23:48 +0100

    deps: update demo payment service

--- src/modules/payment/services/demo.payment.service.ts
+++ src/modules/payment/services/demo.payment.service.ts
@@ -2,31 +2,31 @@ import { v4 as uuidv4 } from "uuid";
 import { IPaymentServiceProvider, MockResponse } from "../base.payment.service";
 
 export class DemoPaymentProcessor implements IPaymentServiceProvider {
-  chargeSource(amount: number, token: string): Promise<{ amount: number; token: string; status: "success" }> {
-    return this.simulateDelayedProcessing({ amount, token, status: "success" });
+  chargeSource(amount: number, token: string): Promise<MockResponse> {
+    return this.simulateDelayedProcessing({ amount, token, status: "success" }, "Insufficient Balance💸");
   }
 
   payoutToBank(amount: number, token: string): Promise<MockResponse> {
-    return this.simulateDelayedProcessing({ amount, token, status: "success" });
+    return this.simulateDelayedProcessing({ amount, token, status: "success" }, "Issuer or Switch Inoperative🤣");
   }
 
   tokenizeCard(...args: any): Promise<string> {
     const token = uuidv4();
-    return this.simulateDelayedProcessing(token);
+    return this.simulateDelayedProcessing(token, "cannot reach tokenization service");
   }
 
   tokenizeBankAccount(...args: any): Promise<string> {
     const token = uuidv4();
-    return this.simulateDelayedProcessing(token);
+    return this.simulateDelayedProcessing(token, "cannot reach tokenization service");
   }
 
-  private simulateDelayedProcessing<T>(mockDataResponse: T): Promise<T> {
+  private simulateDelayedProcessing<T>(mockDataResponse: T, errorMessage?: string): Promise<T> {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         const status = Math.random() * 10;
         if (status < 5) resolve(mockDataResponse);
 
-        reject("Insufficient Balance");
+        reject(errorMessage);
       }, Math.random());
     });
   }
