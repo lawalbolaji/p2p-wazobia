8c906f22c43011d71350bdb7fc2a0ec2375c6f3a
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2023-04-20 15:23:18 +0100

    auth: update hashing algo for jwt and move all jwt creation to the auth service

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -4,7 +4,7 @@ import debug from "debug";
 import * as bcrypt from "bcrypt";
 
 const jwtSecret: string = process.env.JWT_SECRET!!;
-const tokenExpirationInSeconds = 36000;
+const tokenExpirationInSeconds = process.env.TTL || 36000;
 const log: debug.IDebugger = debug("app:auth-controller");
 
 export class AuthService {
@@ -12,12 +12,13 @@ export class AuthService {
 
   async createJWT(data: { userId: string }) {
     try {
-      const refreshId = data + jwtSecret;
+      const refreshId = data.userId + jwtSecret;
       const salt = crypto.createSecretKey(crypto.randomBytes(16));
       const hash = crypto.createHmac("sha512", salt).update(refreshId).digest("base64");
 
       const refreshKey = salt.export();
       const token = jwt.sign({ userId: data.userId, refreshKey }, jwtSecret, {
+        algorithm: "HS256",
         expiresIn: tokenExpirationInSeconds,
       });
 
